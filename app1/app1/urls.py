5ec95e90fd3371b3822d514171ed7affa533ca2c
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-08-29 11:47:56 -0600

    Update urls.py

--- app1/app1/urls.py
+++ app1/app1/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from django.urls import path
+
+def home_page_view(request):
+	return HttpResponse("<h1>World's Simplest Website</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page_view),
 ]
