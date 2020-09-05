9e94c1a646836d27981d544bcb8681fb9b5423c7
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-09-04 22:21:44 -0600

    update

--- Week 2/Week_2_App/Week_2_App/urls.py
+++ Week 2/Week_2_App/Week_2_App/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from page.views import AboutView, HomeView, ProfileView
+from pages.views import AboutView, HomeView, ProfileView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
