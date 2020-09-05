a6f4c25be03edd0cd2af960ec01b658d9f4186c9
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-09-04 22:42:28 -0600

    update views

--- Week 2/Week_2_App/pages/views.py
+++ Week 2/Week_2_App/pages/views.py
@@ -2,10 +2,28 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class AboutView(TemplateView):
-    template_name = 'about.html'
+    template_name = 'page.html'
+    
+    def get_context_data(self, **kwargs):
+        return{
+            'title': 'About Page',
+            'body': 'Once upon a time ...',
+        }
     
 class HomeView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'page.html'
+    
+    def get_context_data(self, **kwargs):
+        return{
+            'title': 'Home Page',
+            'body': 'Once upon a time ...',
+        }
     
 class ProfileView(TemplateView):
-    template_name = 'profile.html'
+    template_name = 'page.html'
+    
+    def get_context_data(self, **kwargs):
+        return{
+            'title': 'Profile Page',
+            'body': 'Once upon a time ...',
+        }
