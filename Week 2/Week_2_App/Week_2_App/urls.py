cf092773810ddcd9a7497946381ef338e21668b2
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-09-04 22:13:18 -0600

    add views

--- Week 2/Week_2_App/Week_2_App/urls.py
+++ Week 2/Week_2_App/Week_2_App/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from page.views import AboutView, HomeView, ProfileView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about', AboutView.as_view()),
+    path('home', HomeView.as_view()),
+    path('profile', ProfileView.as_view()),
+    path('', HomeView.as_view()),
 ]
