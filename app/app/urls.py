52e82f79a740531ea78e4d53756fbbfede472f8d
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-09-09 21:46:53 -0600

    Update urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 from django.urls import path
 
 def home_page_view(request):
-	return HttpResponse("<h1>Hello world!!!</h1><p>Welcome to my first app on Python Anywhere.</p>")
+	return HttpResponse("<h1>Hello world!!!</h1><p>Welcome to my first app on Python Anywhere.</p>
+                        <img src="\static\images\superheroes.png" alt="Superheroes">")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
