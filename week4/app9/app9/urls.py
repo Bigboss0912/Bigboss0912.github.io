2e11b402107e0d3305e2194b26459c13a74e6e3b
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-09-14 14:59:58 -0600

    update

---
+++ week4/app9/app9/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+class PageView(TemplateView):
+
+    def get_template_names(self):
+        return self.kwargs('template')
+    
+
+urlpatterns = [
+    path('<str:template>', PageView.as_view()),
+]
