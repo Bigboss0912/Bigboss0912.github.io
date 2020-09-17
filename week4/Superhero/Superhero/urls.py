d737f7607caeb656224069827cbee474d89c80aa
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-09-17 11:44:58 -0600

    update

--- week4/Superhero/Superhero/urls.py
+++ week4/Superhero/Superhero/urls.py
@@ -1,21 +1,19 @@
-"""Superhero URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from hero.views import HeroView
+from django.views.generic import TemplateView
+
+class PageView(TemplateView):
+
+    def get_template_names(self):
+        template_name = self.kwargs
+        .get('template', 'missing.html')
+        if not exists('templates/' + template_name):
+            template_name = 'missing.html'
+        return template_name
+    
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', HeroView.as_view()),
+    path('<str:identity', HeroView.as_view()),
+    path('<str:template>', PageView.as_view()),
 ]
