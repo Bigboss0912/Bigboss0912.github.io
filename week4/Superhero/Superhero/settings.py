da6fb3521138236cb72bff241bd3129afc2cc847
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-09-17 11:46:12 -0600

    Update settings.py

--- week4/Superhero/Superhero/settings.py
+++ week4/Superhero/Superhero/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '$oc9=tkeel4lc+kj5*+bvrbckm1#j0ti1zftr6045j2m-ku(a+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Michael1586.pythonanywhere.com']
 
 
 # Application definition
