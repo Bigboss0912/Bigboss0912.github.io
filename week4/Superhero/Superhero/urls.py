1fd463a3f5a8474b43c9ac34221533e8a1523fa1
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-09-18 15:45:58 -0600

    update

--- week4/Superhero/Superhero/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from hero.views import HeroView, IndexView
-
-
-urlpatterns = [
-    path('', IndexView.as_view()),
-    path('<str:identity>', HeroView.as_view()),
-]
