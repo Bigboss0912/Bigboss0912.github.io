d936906d0d9cd63833ea539a505243d50ebf8864
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-09-27 20:39:54 -0600

    update hero/test

--- Superhero/hero/tests.py
+++ Superhero/hero/tests.py
@@ -10,9 +10,10 @@ class HeroTests(TestCase):
         self. assertEqual(len(Superhero.objects.all()), 0)
         
     def test_create(self):
-        Superhero.objects.create()
+        Superhero.objects.create(name'abcd', identity='1234')
         for s in Superhero.objects.all():
-            print(s.name)
-        self. assertEqual(len(Superhero.objects.all()), 0)
+            print("SUPER", s.name)
+        self. assertEqual(len(Superhero.objects.all()), 1)
+        self. assertEqual(Superhero.objects.get(pk=1).name, 'dflasdjflkh')
     
     
