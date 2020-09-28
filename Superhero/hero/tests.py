001239636de2c10f0a77ccea9e2045c2962d6420
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-09-27 20:46:54 -0600

    update hero\test

--- Superhero/hero/tests.py
+++ Superhero/hero/tests.py
@@ -7,13 +7,15 @@ from .models import Superhero
 class HeroTests(TestCase):
     
     def test_hero_model(self):
-        self. assertEqual(len(Superhero.objects.all()), 0)
+        self.assertEqual(len(Superhero.objects.all()), 0)
         
     def test_create(self):
-        Superhero.objects.create(name='abcd', identity='1234')
-        self. assertEqual(len(Superhero.objects.all()), 1)
-        self. assertEqual(Superhero.objects.get(pk=1).name, 'abcd')
-        self. assertEqual(Superhero.objects.get(pk=1).identity, '1234')
-    
-    
-    
+        Superhero.objects.create(name='Hulk', identity='Bruce Banner')
+        self.assertEqual(len(Superhero.objects.all()), 1)
+        self. assertEqual(Superhero.objects.get(pk=1).name, 'Hulk')
+        self. assertEqual(Superhero.objects.get(pk=1).identity, 'Bruce Banner')
+        
+    def test_update(self):
+        Superhero.objects.create(name='Hulk', identity='Bruce Banner')
+        x = Superhero.objects.get(pk=1)
+        self. assertEqual(x.name, 1)
