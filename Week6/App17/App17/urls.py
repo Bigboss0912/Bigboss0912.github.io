d39daed8d9ac36aec2e3d57b9c7801433a035a9b
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-10-02 15:05:29 -0600

    update urls

--- Week6/App17/App17/urls.py
+++ Week6/App17/App17/urls.py
@@ -5,6 +5,12 @@ from django.views.generic import TemplateView
 
 class HeroView(TemplateView):
     template_name="hero.html"
+        
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'My Hero View', 
+            'body': 'Heros are awesome!',
+        }
 
 urlpatterns = [
     path('', HeroView.as_view()),
