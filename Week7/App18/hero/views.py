ec0b71b5dcc5e4889b920640899cb9eafd9bfa17
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-10-05 14:57:33 -0600

    update

--- Week7/App18/hero/views.py
+++ Week7/App18/hero/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class HeroView(TemplateView):
+    template_name="hero.html"
+        
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Superhero List',
+            'name': 'Iron Man', 
+            'identity': 'Tony Stark',
+        }
