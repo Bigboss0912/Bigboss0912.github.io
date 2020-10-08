07d7bfbfb02965e541948a4fb4de223db8957ad9
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-10-08 13:38:48 -0600

    update

--- Superhero/Superhero/urls.py
+++ Superhero/Superhero/urls.py
@@ -1,11 +1,7 @@
-from django.contrib import admin
+from hero.views import AboutPage, BasePage, HeroView
 from django.urls import path
-from hero.views import HeroView, IndexView
-
-
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', IndexView.as_view()),
+    path('', HeroView.as_view()),
     path('<str:identity>', HeroView.as_view()),
 ]
--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from .models import Superhero
 
 class HeroView(TemplateView):
     template_name = "hero.html"
 
     def get_context_data(self, **kwargs):
-        id = kwargs.get('identity', 'Ironman')
-        return{'hero' : id}
+        heroes = Superhero.objects.all()
+        return {'heroes': heroes, 'css': '/static/hero.css'}
+
+class AddHeroView(CreateView):
+    template_name = "hero_add.html"
+    model = Superhero    
 
 class IndexView(TemplateView):
     template_name = "index.html"
