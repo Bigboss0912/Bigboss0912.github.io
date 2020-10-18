cbac048677528973fa9c56123727dc87db75455e
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-10-18 14:39:35 -0600

    update

--- Superhero/hero/models.py
+++ Superhero/hero/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
 
 class Superhero(models.Model):
     name = models.CharField(max_length=20)
@@ -8,5 +8,7 @@ class Superhero(models.Model):
     image = models.CharField(max_length=200, null=True)
     
     def __str__(self):
-        return f'{self.name} - {self.identity}'
-    
+        return self.identity
+
+    def get_absolute_url(self): 
+        return reverse('hero_detail', args=[str(self.id)])
--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -8,13 +8,13 @@ class HeroDetailView(DetailView):
     template_name = "hero_detail.html"
     model = Superhero
     
-    def get_context_data(self, **kwargs):
-        kwargs = super().get_context_data(**kwargs)
-        image = kwargs['object'].image
-        if not exists('static/' + images)
-            kwargs['missing'] = True
-        else:
-            return kwargs
+    #def get_context_data(self, **kwargs):
+        #kwargs = super().get_context_data(**kwargs)
+        #image = kwargs['object'].image
+        #if not exists('static/' + images):
+            #kwargs['missing'] = True
+        #else:
+            #return kwargs
     
     
 class HeroListView(ListView):
@@ -27,13 +27,9 @@ class HeroAddView(CreateView):
     model = Superhero
     fields = '__all__'
     
-    
+        
 class HeroEditView(UpdateView):
     template_name = "hero_edit.html"
     model = Superhero
     fields = '__all__'
     
-    def get_context_data(self, **kwargs):
-        kwargs = super(HeroEditView, self).get_context_data(**kwargs)
-        kwargs['edit'] = True
-        return kwargs
