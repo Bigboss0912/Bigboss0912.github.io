c88d5ff4bce81d0ba86d58a09ec14cbec19d35e3
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-10-19 16:36:14 -0600

    update

---
+++ Superhero/hero/views.py
@@ -0,0 +1,35 @@
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView
+from .models import Superhero
+
+
+class HeroDetailView(DetailView):
+    template_name = "hero_detail.html"
+    model = Superhero
+    
+    #def get_context_data(self, **kwargs):
+        #kwargs = super().get_context_data(**kwargs)
+        #image = kwargs['object'].image
+        #if not exists('static/' + images):
+            #kwargs['missing'] = True
+        #else:
+            #return kwargs
+    
+    
+class HeroListView(ListView):
+    template_name = "hero_list.html"
+    model = Superhero
+    
+    
+class HeroAddView(CreateView):
+    template_name = "hero_add.html"
+    model = Superhero
+    fields = '__all__'
+    
+        
+class HeroEditView(UpdateView):
+    template_name = "hero_edit.html"
+    model = Superhero
+    fields = '__all__'
+    
