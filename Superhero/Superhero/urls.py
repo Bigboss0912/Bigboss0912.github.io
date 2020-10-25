18079fb1ee20e724d07f2f7f6e9b1768c8ad8038
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-10-24 23:17:58 -0600

    Update urls.py

--- Superhero/Superhero/urls.py
+++ Superhero/Superhero/urls.py
@@ -1,25 +1,37 @@
 
-#from hero.views import HeroView, HeroListView, AddHeroView, HeroDetailView
-#from django.urls import path
-
+#from hero.views import HeroAddView, HeroDetailView, HeroEditView, HeroListView
+#from django.urls import path, include
+#from django.contrib import admin
+#
+#
 #urlpatterns = [
-    #path('', HeroView.as_view()),
-    #path('<str:identity>', HeroView.as_view()),
+#    
+#    path('', HeroListView.as_view(), name='hero_list'),
+#    path('<int:pk>', HeroDetailView.as_view(), name='hero_detail'),
+#    path('add',  HeroAddView.as_view(),    name='hero_add'),
+#    path('<int:pk>/', HeroEditView.as_view(), name='hero_edit'),
+#    path('admin/', admin.site.urls),
+#    path('accounts/', include('django.contrib.auth.urls')),
+#    path('accounts/', include('accounts.urls')),
+#    
 #]
 
-from hero.views import HeroAddView, HeroDetailView, HeroEditView, HeroListView
-from django.urls import path, include
-from django.contrib import admin
+from django.urls import path
 
+from .views import (
+    HeroListView,
+    HeroDetailView,
+    HeroCreateView,
+    HeroUpdateView,
+    HeroDeleteView,
+)
 
 urlpatterns = [
     
-    path('', HeroListView.as_view(), name='hero_list'),
-    path('<int:pk>', HeroDetailView.as_view(), name='hero_detail'),
-    path('add',  HeroAddView.as_view(),    name='hero_add'),
-    path('<int:pk>/', HeroEditView.as_view(), name='hero_edit'),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('post/<int:pk>/delete', HeroDeleteView.as_view(), name='hero_delete'),
+    path('post/add',             HeroCreateView.as_view(), name='hero_add'),
+    path('post/<int:pk>',        HeroDetailView.as_view(), name='hero_detail'),
+    path('post/<int:pk>/',       HeroUpdateView.as_view(), name='hero_edit'),
+    path('',                     HeroListView.as_view(),   name='hero_list'),
     
 ]
