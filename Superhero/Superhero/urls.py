0a459f5c25589271ac9c4e518ac6f102bb43f79b
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-10-25 00:02:10 -0600

    update

---
+++ Superhero/Superhero/urls.py
@@ -0,0 +1,27 @@
+
+#from hero.views import HeroAddView, HeroDetailView, HeroEditView, HeroListView
+#from django.urls import path, include
+#from django.contrib import admin
+#
+#
+#urlpatterns = [
+#    
+#    path('', HeroListView.as_view(), name='hero_list'),
+#    path('<int:pk>', HeroDetailView.as_view(), name='hero_detail'),
+#    path('add',  HeroAddView.as_view(),    name='hero_add'),
+#    path('<int:pk>/', HeroEditView.as_view(), name='hero_edit'),
+#    path('admin/', admin.site.urls),
+#    path('accounts/', include('django.contrib.auth.urls')),
+#    path('accounts/', include('accounts.urls')),
+#    
+#]
+
+from django.urls import path, include
+
+
+urlpatterns = [
+    
+    path('', include('accounts.urls')), 
+    path('', include('hero.urls')),
+    
+]
