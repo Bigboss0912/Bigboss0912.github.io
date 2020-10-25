f9c9802d075ae5345b5df20b5276cd26255c0b22
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-10-24 22:14:25 -0600

    update

--- Superhero/accounts/views.py
+++ Superhero/accounts/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 
-class SignUpView(generic.CreateView):
+class RegisterView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
