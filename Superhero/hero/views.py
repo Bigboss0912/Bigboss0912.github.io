3ec2dc28f6c6ea649517f239486a8ce83754d86e
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-11-09 15:02:51 -0700

    Update views.py

--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -1,39 +1,3 @@
-#from django.urls import reverse_lazy
-#from django.contrib.auth.mixins import LoginRequiredMixin
-#from django.views.generic import DetailView, ListView, TemplateView
-#from django.views.generic.edit import CreateView, UpdateView, DeleteView
-#from os.path import exists
-#from .models import Superhero
-#
-#
-#class HeroDetailView(DetailView):
-#    template_name = "hero_detail.html"
-#    model = Superhero   
-#    
-#class HeroListView(ListView):
-#    template_name = "hero_list.html"
-#    model = Superhero
-#    
-#    
-#class HeroAddView(LoginRequiredMixin, CreateView):
-#    template_name = "hero_add.html"
-#    model = Superhero
-#    fields = '__all__'
-#    
-#    def form_valid(self, form):
-#        form.instance.author_id = self.request.user.pk
-#        return super().form_valid(form)
-#    
-#        
-#class HeroEditView(LoginRequiredMixin, UpdateView):
-#    template_name = "hero_edit.html"
-#    model = Superhero
-#    fields = '__all__'
-#    
-#class HeroDeleteView(LoginRequiredMixin, DeleteView): 
-#    model = Superhero
-#    template_name = 'hero_delete.html'
-#    success_url = reverse_lazy('hero_list')
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
@@ -41,6 +5,8 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Superhero
+
+from .hero import accordion_data, card_data, cards_data, markdown_file_data, table_data, tabs_data
 
 
 class HeroListView(ListView):
