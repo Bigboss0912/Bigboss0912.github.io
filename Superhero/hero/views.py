1cbab0ca1f0164145a96c07c4c74be95c95f6c63
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-12-01 19:16:16 -0700

    Update views.py

--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView 
 from django.urls import reverse_lazy
-from markdown import markdown
 
 from .models import Superhero
 
