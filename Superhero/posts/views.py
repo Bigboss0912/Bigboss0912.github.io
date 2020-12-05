d17149def5655436a0082bc60b4a00ceff78ee44
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-12-04 23:26:16 -0700

    Update views.py

--- Superhero/posts/views.py
+++ Superhero/posts/views.py
@@ -8,3 +8,35 @@ from .models import Post
 class NewsView(TemplateView):
     model = Post
     template_name = 'news.html'
+    
+
+class NewsListView(ListView):
+    model = Post
+    template_name = 'news_list.html'
+
+
+class NewsDetailView(DetailView):
+    model = Post
+    template_name = 'news_detail.html'
+
+    
+class NewsCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'news_add.html'
+    fields = '__all__'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.pk
+        return super().form_valid(form)
+    
+
+class NewsUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'news_edit.html'
+    fields = '__all__'
+    
+
+class NewsDeleteView(LoginRequiredMixin, DeleteView): 
+    model = Post
+    template_name = 'news_delete.html'
+    success_url = reverse_lazy('news_list')
