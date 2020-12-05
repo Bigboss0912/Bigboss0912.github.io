cacdfa70ae7bdad29e56fd83f08ccd7feb2e11ef
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-12-04 23:31:20 -0700

    Update models.py

--- Superhero/posts/models.py
+++ Superhero/posts/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
-class Post(models.Model):
-    text = models.TextField()
+
+class Superhero(models.Model):
+    Author = models.ForeignKey(User)
+    Date = models.DateField()
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Hero = models.ForeignKey(Superhero)
+    
+    def __str__(self):
+        return self.identity
+
+    def get_absolute_url(self): 
+        return reverse('news_detail', args=[str(self.id)])
