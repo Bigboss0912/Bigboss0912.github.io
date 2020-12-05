03c6232b28c9cf915121ee77d6ddc09bc6e5099c
Bigboss0912 <60019497+Bigboss0912@users.noreply.github.com>
2020-12-04 23:07:02 -0700

    Update views.py

--- Superhero/posts/views.py
+++ Superhero/posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView 
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Superhero
+
+
+class NewsView(TemplateView):
+    model = Post
+    template_name = 'news.html'
